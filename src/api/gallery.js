27c9a956707e7fd43c729020d0129a5133e84224
rooddeeo <rooddeeo@gmail.com>
2023-11-19 15:31:37 +0200

    React Home Work 3

--- src/api/gallery.js
+++ src/api/gallery.js
@@ -1,7 +1,18 @@
+// import axios from 'axios';
+// axios.defaults.url =
+//   'https://pixabay.com/api/?q=cat&page=1&key=12002814-5debf547df742213b695907de&image_type=photo&orientation=horizontal&per_page=12';
+// export const getGallery = async () => {
+//   const { data } = await axios();
+//   return data;
+// };
+
 import axios from 'axios';
-axios.defaults.url =
-  'https://pixabay.com/api/?q=cat&page=1&key=12002814-5debf547df742213b695907de&image_type=photo&orientation=horizontal&per_page=12';
-export const getGallery = async () => {
-  const { data } = await axios();
+axios.defaults.url = 'https://pixabay.com/api/';
+const getGallery = async () => {
+  const { data } = await axios.get(
+    `?q=cat&page=1&key=12002814-5debf547df742213b695907de&image_type=photo&orientation=horizontal&per_page=12`
+  );
   return data;
 };
+
+export default getGallery
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,47 @@
 import React, { Component } from 'react';
-import Loader from './Loader/Loader';
-// import Searchbar from './Searchbar/Searchbar';
-import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
-import ImageGallery from './ImageGallery/ImageGallery';
 
-import Button from './Button/Button';
-import Modal from './Modal/Modal';
+import getGallery from '../api/gallery.js';
+// import Loader from './Loader/Loader';
+// import Searchbar from './Searchbar/Searchbar';
+// import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
+// import ImageGallery from './ImageGallery/ImageGallery';
+//import axios from 'axios';
+// import Button from './Button/Button';
+// import Modal from './Modal/Modal';
+
+// class App extends Component {
+//   onSubmit = () => {};
+
+//   render() {
+//     return (
+//       <div>
+//         <Loader />
+//         {/* <Searchbar onSubmit={onSubmit} /> */}
+//         <ImageGalleryItem />
+//         <ImageGallery />
+//         <Button />
+//         <Modal />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
 
 class App extends Component {
-  onSubmit = () => {};
+  async componentDidMount() {
+   const data = await getGallery()
+   console.log(data);
+ }
 
-  render() {
-    return (
-      <div>
-        <Loader />
-        {/* <Searchbar onSubmit={onSubmit} /> */}
-        <ImageGalleryItem />
-        <ImageGallery />
-        <Button />
-        <Modal />
-      </div>
-    );
-  }
-}
 
-export default App;
+   render() {
+     return (
+       <div>
+       work
+       </div>
+     );
+   }
+ }
+
+ export default App;
